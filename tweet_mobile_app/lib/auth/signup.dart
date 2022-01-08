30d222907c427a298fac5e4521e7f9d6cd8a870a
Kaisen Ye <75810017+kaisenye@users.noreply.github.com>
2022-01-08 13:06:21 -0500

    connect firebase flutter

---
+++ tweet_mobile_app/lib/auth/signup.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart'; 
+
+class SignUp extends StatefulWidget {
+  SignUp({Key? key}) : super(key: key);
+
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  // render function
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+    );
+  }
+}
